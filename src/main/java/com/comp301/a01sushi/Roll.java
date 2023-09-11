2e98223d5d407cf74d628c7e1eb2e4cd000b70e4
Ethan Byrd <ethbyr@ad.unc.edu>
2023-09-11 10:14:44 -0400

    Roll Jedi

--- src/main/java/com/comp301/a01sushi/Roll.java
+++ src/main/java/com/comp301/a01sushi/Roll.java
@@ -17,14 +17,13 @@ public class Roll implements Sushi{
             }
         }
         IngredientPortion[] a = roll_ingredients.clone();
-        /*for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length; i++) {
             for (int j = i; j < a.length; j++) {
                 if (a[i].getName() == a[j].getName()) {
                     a[i].combine(a[j]);
-                    a[j] = null;
-                    for (int k = j; k< a.length; k++) {
-                        if (a[k+1] != null){
-                        a[k] = a[k+1];}
+                    }
+                }
+            }
 
         int s = 0;
         for (int i = 0; i < a.length; i++) {
@@ -33,9 +32,8 @@ public class Roll implements Sushi{
                     a[i].combine(new SeaweedPortion(0.1 - a[i].getAmount()));
                     s += 1;
                 }
-            }
+            }}
         if (s == 0) {a[a.length-1] = new SeaweedPortion(0.1);}
-        }*/
         this.ingredients = a;
     }
     @Override
